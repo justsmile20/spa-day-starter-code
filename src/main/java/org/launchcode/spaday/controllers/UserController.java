6375200bfd5522f6b674a6410585d31695f8d04e
Chigozie Philip <51035644+justsmile20@users.noreply.github.com>
2021-02-26 13:04:29 -0500

    added date user joined

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,56 @@
+package org.launchcode.spaday.controllers;
+
+
+import org.launchcode.spaday.data.UserData;
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(){
+        return "/user/add";
+    }
+
+
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+        // add form submission handling code here
+
+        // validate password match
+        if(verify.equals(user.getPassword())){
+            // is true
+            //view template with a message that welcomes the user by username.
+            UserData.addUser(user);
+            model.addAttribute("user", user);
+            model.addAttribute("users", UserData.getAll());
+            return "user/index";
+        } else{
+            //false
+            model.addAttribute("error", "Your password did not match");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("username", user.getEmail());
+            return "user/add";
+        }
+
+    }
+
+    @GetMapping("details/{userId}")
+    public String displayUser(Model model, @PathVariable int userId){
+        User user = UserData.getById(userId);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-hh:mm:ss");
+        String strDate = dateFormat.format(user.getJoinedDate());
+        model.addAttribute("joined", strDate);
+        return "user/details";
+    }
+}
---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,55 @@
+package org.launchcode.spaday.models;
+
+import java.util.Date;
+
+public class User {
+    private static int nextId = 1;
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private final Date joinedDate;
+
+
+    //constructors
+    public User(){
+        id = nextId;
+        nextId++;
+        this.joinedDate = new Date();
+    }
+
+    // getters & setters
+
+
+    public Date getJoinedDate() {
+        return this.joinedDate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
